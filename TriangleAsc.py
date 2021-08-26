d1b6f82af4c6731d69748966cde64caefc61a222
Gavin Fu <gavin.fu@mobinology.com>
2021-08-26 23:14:31 +0800

    first commit

---
+++ TriangleAsc.py
@@ -0,0 +1,86 @@
+
+from scipy.signal import argrelextrema
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+
+class TriangleAsc:
+    def __init__(self,order=5,m=150,recursion=True,reduceTo=5):
+        '''
+        order:How many points on each side to use for the comparison to consider comparator(n, n+x) to be True.
+        m: starts from
+        reduceTo : recurrsively find extrema until redeceTo is met
+        '''
+        self.model_localmin = LinearRegression()
+        self.model_localmax = LinearRegression()
+        self.order = order
+        self.m = m 
+        self.recursion = recursion
+        self.reduceTo = reduceTo
+
+    def findExtrema(self, x,y , mode='max',order=1, recursion=True):
+        if mode == 'min':
+            self.comparatator = np.less
+        else:
+            self.comparatator = np.greater
+        localextrema = argrelextrema(np.array(y), self.comparatator,order=order)[0]
+        localext_x = []
+        localext_y = []
+        for e in localextrema:
+            localext_x.append(x[e])
+            localext_y.append(y[e])
+
+        if recursion and len(localextrema) > self.reduceTo:
+            localext_x,localext_y = self.findExtrema(localext_x,localext_y,mode=mode,recursion=True)
+
+
+    
+        return localext_x,localext_y
+
+    def Classify(self,y):
+        #original
+        x = np.linspace(1,len(y),len(y))
+        #m= np.argmin(y)
+
+        localmin_x,localmin_y = self.findExtrema(x,y,mode='min',recursion=self.recursion, order=self.order)
+        localmax_x,localmax_y = self.findExtrema(x,y,mode='max',recursion=self.recursion, order=self.order)
+
+
+        self.localmin_x = localmin_x
+        self.localmin_y = localmin_y
+
+        self.localmax_x = localmax_x
+        self.localmax_y=localmax_y
+
+        m = self.m
+        self.model_localmin.fit(np.array(localmin_x[-m:]).reshape(-1,1),np.array(localmin_y[-m:]).reshape(-1,1))
+        self.model_localmax.fit(np.array(localmax_x[-m:]).reshape(-1,1),np.array(localmax_y[-m:]).reshape(-1,1))
+
+
+        return self.model_localmin.coef_[0][0] > self.model_localmax.coef_[0][0] and (self.model_localmin.coef_[0][0]>0)
+
+    def Plot(self,y,title=''):
+
+        fig, ax = plt.subplots()
+        x = np.linspace(1,len(y),len(y))
+        plt.plot(x,y)
+
+        plt.title(title)
+        x_new = np.linspace(self.m,len(y)-1,len(y)-self.m)
+        y_new_min = self.model_localmin.predict(x_new[:, np.newaxis])
+        y_new_max = self.model_localmax.predict(x_new[:, np.newaxis])
+        
+
+        
+        plt.plot(x_new, y_new_min,label='support')
+        plt.plot(x_new, y_new_max,label='resistance')
+        plt.legend()
+        return fig, ax
+
+
+
+
+
+
+# %%
---
+++ main.py
@@ -0,0 +1,134 @@
+import streamlit as st
+from TriangleAsc import TriangleAsc
+from pandas_datareader import data as pdr
+import yfinance as yf
+import datetime
+yf.pdr_override()
+
+
+
+# Title
+st.title("Triangular Ascending")
+
+
+# Sidebar input
+st.sidebar.title("Ticker")
+symbol = st.sidebar.text_input("Symbol")
+region = st.sidebar.selectbox(
+'Region',
+('HK', 'US'))
+
+if region == 'HK':
+    symbol = str(symbol).zfill(4) + '.HK' 
+
+
+
+# Sidebar clsf setting 
+st.sidebar.markdown(
+'''
+# Alorithm Parameters
+- order:How many points on each side to use for the comparison to consider comparator(n, n+x) to be True.
+- m: starts from
+- reduceTo : recurrsively find extrema until redeceTo is met
+'''
+)
+order = st.sidebar.slider("Order",min_value=1,max_value=50,value=20)
+m = st.sidebar.slider("m",min_value=100,max_value=250,value=150,step=10)
+reduceTo = st.sidebar.slider("reduceTo",min_value=1,max_value=10,value=5,step=1)
+recursion=st.sidebar.checkbox("recursion",value=True)
+
+if st.sidebar.button('Reset Default'):
+    order=20
+    m=150
+    reduceTo=5
+
+
+if (symbol=="0000.HK"):
+    st.markdown(
+        '''
+        ## Input Ticker to Begin!
+        '''
+    )
+
+else:
+    #yahoo finance API
+    @st.cache
+    def fetch_yahoo(symbol):
+        start_date = datetime.datetime.now() - datetime.timedelta(days=365)
+        end_date = datetime.date.today()
+        yahoo_data = pdr.get_data_yahoo(symbol, start=start_date, end=end_date)
+        return yahoo_data
+
+    yahoo_data = fetch_yahoo(symbol)
+    st.dataframe(yahoo_data)
+
+    if len(yahoo_data['Close'])==0:
+        st.markdown(
+            '''
+            ## No Record found!
+            '''
+        )
+
+    else:
+
+        # load model
+        clsf = TriangleAsc(order=order,m=m,recursion=False,reduceTo=reduceTo)
+
+        # if symbol in priceDict:
+        # Classify
+        isTrgAsc = clsf.Classify(yahoo_data['Close'])
+
+
+        # Plot
+        fig, ax = clsf.Plot(yahoo_data['Close'],symbol)
+        st.pyplot(fig)
+
+
+        #Results
+        st.write("Is triangular ascending:")
+        st.write(isTrgAsc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+# clsf = TriangleAsc(order=20,m=150,recursion=False,reduceTo=5)
+
+# priceDict = {}
+
+# for d in data:
+#     try:
+#         symbol, close, *_ = d.split(':')
+#         symbol, close, *_ = d.split(':')
+#         close = json.loads(close)
+#         priceDict[symbol] = close
+#     except Exception as e :
+#         print(e)       
+# for symbol in filterList['Stock']:
+#     try:
+#         close = priceDict[symbol]
+#         clsf.Classify(close)
+#         clsf.Plot(close,title=symbol)
+#     except Exception as e: 
+#         print(e)
+# #%%
+
+# clsf.Plot(priceDict['0558.HK'],title=symbol)
+
+
